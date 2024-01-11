126537037d318504fd462e493f2a92ad838d07a1
kanyi <jkanyi757@gmail.com>
2024-01-11 16:16:48 +0300

    fix linter errors

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get authenticated_root_url
     assert_response :success
   end
