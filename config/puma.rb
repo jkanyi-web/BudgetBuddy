8a4407e87388d310c10e78a5bafb05327a2007d9
kanyi <jkanyi757@gmail.com>
2024-03-04 16:07:38 +0300

    render deployment setup

--- config/puma.rb
+++ config/puma.rb
@@ -33,3 +33,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+
+preload_app!
